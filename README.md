da7f456355bca7683ea746131e38bd3eb4577b56
Louis Manière <louismaniere@orange.fr>
2024-10-25 12:12:59 +0200

    get fortune function

--- README.md
+++ README.md
@@ -1,2 +1,20 @@
 # heroscope
 L'horoscope des héros, un dépôt pour jouer avec git et github
+
+``` python
+# go to the working folder you want to download the mapdo application
+cd Path/to/my/folder
+# copy mapdo repository with git
+git clone https://github.com/LouisManiere/heroscope.git
+# create a new virtual environnement in python 3
+python -m venv env --prompt heroscope
+# activate your new environment
+# Windows
+.\env\Scripts\activate
+# Linux
+source ./env/bin/activate
+# update pip
+python -m pip install -U pip
+# install all others dependencies
+pip install -r requirements.txt
+```
---
+++ heroscope.py
@@ -0,0 +1,12 @@
+import random
+
+def get_fortune():
+    """Return a random fortune for super hero"""
+    return random.choice([
+        "Votre courage et votre détermination vous mèneront vers une nouvelle mission risquée. Un puissant allié pourrait se manifester, mais gardez votre sang-froid, car même un héros doit parfois tempérer son feu intérieur",
+        "Vous êtes sur le point de découvrir un secret qui pourrait changer le cours de l'histoire. Restez vigilant et ne faites confiance à personne, car les ennemis sont partout. Votre force intérieure vous guidera vers la vérité",
+        "Un mystérieux artefact pourrait bientôt croiser votre chemin. Soyez prudent, car il pourrait s'agir d'une arme redoutable. Votre intelligence et votre ruse seront vos meilleurs atouts pour déjouer les plans de vos ennemis",
+        "Une nouvelle menace plane sur la ville, et vous seul pouvez l'arrêter. Votre sens du devoir et votre loyauté envers vos amis vous permettront de triompher. N'oubliez pas que même les plus grands héros ont parfois besoin d'aide",
+        "Un ancien ennemi refait surface, et cette fois-ci, il est plus puissant que jamais. Vous devrez faire preuve de courage et de détermination pour le vaincre. N'oubliez pas que la véritable force réside dans votre cœur, pas dans vos pouvoirs",
+        "Une nouvelle aventure vous attend, et elle pourrait vous emmener aux confins de l'univers. Restez ouvert d'esprit et prêt à affronter l'inconnu. Votre curiosité et votre soif de justice vous guideront vers la victoire"
+    ])
